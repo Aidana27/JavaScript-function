78d688244c3625113a8435b0d939777f416bc621
Aidana27 <75001581+Aidana27@users.noreply.github.com>
2021-02-12 21:58:45 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function max (arg1, arg2) {
+    var max = Math.max (arg1, arg2)
+    return max;
+}
+
+
+function min (arg1, arg2) {
+    var min = Math.min (arg1, arg2)
+    return min;
+}
+
+
+function qwad (a, n) {
+    var qwad = Math.pow (a, n)
+    return qwad;
+}
+
+console.log(max(3, 6));
+
+console.log(min(3, 6));
+
+console.log(qwad(3, 6))
